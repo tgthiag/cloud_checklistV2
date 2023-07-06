50db59ac68c0d08aa840e247a44d83c24d350756
tgthiag <tgthiag@gmail.com>
2023-07-06 00:02:34 -0300

    Updating project

--- index.js
+++ index.js
@@ -1,14 +1,8 @@
-// import { AppRegistry, Platform } from "react-native";
-// import { registerRootComponent } from "expo";
-// import App from "./App";
-// import { name as appName } from "./app.json";
+import { registerRootComponent } from 'expo';
 
-import { registerRootComponent } from "expo"
-import App from "./App"
+import App from './App';
 
-// // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
-// // It also ensures that whether you load the app in the Expo client or in a native build,
-// // the environment is set up appropriately
-
-//   AppRegistry.registerComponent(appName.toLowerCase(), () => App);
-registerRootComponent(()=>App())
+// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
+// It also ensures that whether you load the app in Expo Go or in a native build,
+// the environment is set up appropriately
+registerRootComponent(App);
