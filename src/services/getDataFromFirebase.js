ab9b32ce515bfb1d091dcb7845e2ba79c3c65ee3
tgthiag <tgthiag@gmail.com>
2023-07-13 22:31:18 -0300

    minor fix

--- src/services/getDataFromFirebase.js
+++ src/services/getDataFromFirebase.js
@@ -3,21 +3,17 @@ import { getDatabase, ref, onValue } from "firebase/database";
 import { dbpath } from "../config/dbpath";
 import { db } from "../../database";
 
-function firebaseOnValue() {
-  const database = getDatabase(db);
+export function useFirebaseData() {
   const [data, setData] = useState(null);
 
   useEffect(() => {
+    const database = getDatabase(db);
     const reference = ref(database, `data/${dbpath}/records`);
     onValue(reference, (snapshot) => {
       const firebaseData = snapshot.val();
       setData(firebaseData);
-      console.log(firebaseData);
     });
-
   }, []);
 
   return data;
 }
-
-export default firebaseOnValue;
