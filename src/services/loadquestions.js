5b16ad422f62f2adfe0feff49374e63faa11c20b
tgthiag <tgthiag@gmail.com>
2023-07-13 22:31:58 -0300

    lints

--- src/services/loadquestions.js
+++ src/services/loadquestions.js
@@ -12,35 +12,10 @@ function loadQuestions() {
     onValue(reference, (snapshot) => {
       const firebaseData = snapshot.val();
       setData(firebaseData);
-      console.log(firebaseData);
     }, { onlyOnce: true });
-
-    // Optionally, you can perform any cleanup logic here if needed
-
   }, []);
 
   return data;
 }
 
 export default loadQuestions;
-
-// import { useEffect, useState } from "react";
-// import { getDatabase, ref, onValue, get } from "firebase/database";
-// import { dbpath } from "../config/dbpath";
-// import {db} from "../../database";
-
-// function loadQuestions() {
-//   const database = getDatabase(db);
-//   const [data, setData] = useState(null);
-
-
-// useEffect(async () => {
-//   const snapshot = await get(ref(database, `data/${dbpath}/questions`))
-//   setData(snapshot)
-//   console.log(data)
-// },[])
-
-//   return data;
-// }
-
-// export default loadQuestions;
